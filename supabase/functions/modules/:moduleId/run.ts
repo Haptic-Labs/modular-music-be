fabfcfd46c6c5350f6b582dc8beb89f37b172053
Haptic Labs <205575443+hapticlabsdev@users.noreply.github.com>
2025-05-27 21:46:47 -0600

    feat(HAP-12): init runner

--- supabase/functions/modules/:moduleId/run.ts
+++ supabase/functions/modules/:moduleId/run.ts
@@ -4,7 +4,10 @@ import {
   setupSupabaseWithServiceRole,
 } from "../../_shared/setup-supabase.ts";
 import { validateAuth } from "../../_shared/validate-auth.ts";
+import { getPlaylistData } from "../../_shared/spotify-data-fetchers/get-playlist-data.ts";
+import { getAlbumData } from "../../_shared/spotify-data-fetchers/get-album-data.ts";
 import { HTTPException } from "@hono/hono/http-exception";
+import { Database } from "../../_shared/database.gen.ts";
 
 type StatusCode = ConstructorParameters<typeof HTTPException>[0];
 
@@ -22,7 +25,8 @@ export const RunModule: HonoFn<"RunModule"> = async (ctx) => {
 
   const { data, error } = await serviceRoleSupabaseClient
     .schema("public")
-    .rpc("GetModuleRunData", { moduleId, callerUserId: user.id });
+    .rpc("GetModuleRunData", { moduleId, callerUserId: user.id })
+    .single();
 
   if (error) {
     const errorCode = parseInt(error.code, 10);
@@ -35,5 +39,72 @@ export const RunModule: HonoFn<"RunModule"> = async (ctx) => {
     });
   }
 
-  // TODO: run module
+  let shouldFetchLikedTracks = false;
+  let recentlyListenedSourceId: string | undefined = undefined;
+  const sourcesWithSpotifyIds = data.moduleSources.reduce<
+    Partial<
+      Record<Database["public"]["Enums"]["SPOTIFY_SOURCE_TYPE"], string[]>
+    >
+  >((acc, source) => {
+    if (source.spotify_id === null) {
+      if (source.type === "LIKED_SONGS") {
+        shouldFetchLikedTracks = true;
+      }
+      if (source.type === "RECENTLY_PLAYED") {
+        recentlyListenedSourceId = source.id;
+      }
+      return acc;
+    }
+    const currentTypeList = acc[source.type] ?? [];
+    acc[source.type] = [...currentTypeList, source.spotify_id];
+    return acc;
+  }, {});
+
+  // TODO: abstract this logic into a shared function
+  // TODO: add better error handling that won't break everything if one fetch fails
+  const allSourceTrackIds: string[] = [];
+  await Promise.allSettled(
+    Object.entries(sourcesWithSpotifyIds).map(async ([typeStr, sourceIds]) => {
+      const type =
+        typeStr as Database["public"]["Enums"]["SPOTIFY_SOURCE_TYPE"];
+      await Promise.allSettled(
+        sourceIds.map(async (spotifyId) => {
+          switch (type) {
+            case "PLAYLIST":
+              allSourceTrackIds.push(
+                ...(
+                  await getPlaylistData({
+                    spotifyId,
+                    userId: user.id,
+                    supabaseClient: serviceRoleSupabaseClient,
+                  })
+                ).data.track_ids,
+              );
+              break;
+            case "TRACK":
+              allSourceTrackIds.push(spotifyId);
+              break;
+            case "ALBUM":
+              allSourceTrackIds.push(
+                ...(
+                  await getAlbumData({
+                    spotifyId,
+                    userId: user.id,
+                    supabaseClient: serviceRoleSupabaseClient,
+                  })
+                ).data.track_ids,
+              );
+              break;
+            case "ARTIST":
+              // TODO: implement (need to implement shared getArtistData)
+              break;
+            default:
+              return;
+          }
+        }),
+      );
+    }),
+  );
+
+  // TODO: implement actions and output
 };
